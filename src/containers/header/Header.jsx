a5b2248e4415d3f8b6a04ec1dc87d9490214b205
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-11-28 16:44:19 +0100

    Internacionalizacion implementada con i18next

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './header.css';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
+  const [t, i18n] = useTranslation("global");
   return (
     <div className='fg__header'>
       Header
+      <h1>{t("header.tagline")}</h1>
+      <button onClick={() => i18n.changeLanguage("es")}>ES</button>
+      <button onClick={() => i18n.changeLanguage("en")}>EN</button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+import global_es from './translations/es/global.json';
+import global_en from './translations/en/global.json';
+
+i18next.init({
+    interpolation: { escapeValue: false},
+    lng: "es",
+    resources: {
+        es: {
+            global: global_es
+        },
+        en: {
+            global: global_en
+        }
+    }
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App/>);
+root.render(
+    <I18nextProvider i18n={i18next}>
+        <App/>
+    </I18nextProvider>
+);
