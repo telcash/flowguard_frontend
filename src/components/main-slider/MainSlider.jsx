12b49586532a2b6ef1914d24a817e5ffc53e8fcb
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-11-29 22:29:32 +0100

    Aplicados estilos a HeroSection

--- src/components/main-slider/MainSlider.jsx
+++ src/components/main-slider/MainSlider.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import './main-slider.css';
 import Carousel from 'react-bootstrap/Carousel';
-import dummyImg from '../../assets/slider-dummy.jpg';
+import dummyImg from '../../assets/KWR Digitalisation & data.jpg';
+import { useTranslation } from 'react-i18next';
 
 const MainSlider = () => {
+    const [t] = useTranslation("global");
     return (
         <div className="fg__mainSlider">
             <Carousel fade>
                 <Carousel.Item className="fg__mainSlider-item">
                     <img src={dummyImg} alt="dummy" />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>{t("header.slides.slide1.title")}</h3>
+                        <p>{t("header.slides.slide1.description")}</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img src={dummyImg} alt="dummy" />
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>{t("header.slides.slide2.title")}</h3>
+                        <p>{t("header.slides.slide2.description")}</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img src={dummyImg} alt="dummy" />
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>{t("header.slides.slide3.title")}</h3>
+                        <p>{t("header.slides.slide3.description")}</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
