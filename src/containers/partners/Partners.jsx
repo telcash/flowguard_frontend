3271dc33ae0d758242e6b53f040ad1f7851442cf
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-11-30 07:12:49 +0100

    Titulos agregados a secciones y modificacion de estilos

--- src/containers/partners/Partners.jsx
+++ src/containers/partners/Partners.jsx
@@ -6,12 +6,15 @@ import airtraceLogo from '../../assets/airtrace-gray.png';
 const Partners = () => {
     return (
         <div className="fg__partners">
-            <a href="https://airtrace.io" target="blank">
-                <img src={airtraceLogo} alt="logo airtrace" />
-            </a>
-            <a href="https://neoradixsolutions.com" target="blank">
-                <img src={neoradixLogo} alt="logo neoradix" />
-            </a>
+            <h1>Nuestros partners</h1>
+            <div className="fg__partners-logos">
+                <a href="https://airtrace.io" target="blank">
+                    <img src={airtraceLogo} alt="logo airtrace" />
+                </a>
+                <a href="https://neoradixsolutions.com" target="blank">
+                    <img src={neoradixLogo} alt="logo neoradix" />
+                </a>
+            </div>
         </div>
     );
 };
