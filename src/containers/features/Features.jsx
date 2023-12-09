26d71d2d92a34784a9397b777854b2970e1c8745
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-09 11:37:41 +0100

    Arreglados estilos en Feature - Cambiado un Feature en Features

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,9 +4,10 @@ import Feature from '../../components/feature/Feature';
 import neuralNetworkIcon from '../../assets/neural-network.png';
 import dataReliabilityIcon from '../../assets/data-quality.png';
 import realTimeIcon from '../../assets/in-time.png';
-import contImprovementIcon from '../../assets/improvement.png';
+//import contImprovementIcon from '../../assets/improvement.png';
 import riskManagementIcon from '../../assets/risk-management.png';
 import environmentImpactIcon from '../../assets/environment.png';
+import electronicsIcon from '../../assets/electronics.png';
 import { useTranslation } from 'react-i18next';
 
 const Features = () => {
@@ -18,7 +19,7 @@ const Features = () => {
         <Feature icon={neuralNetworkIcon} title={t("features.graph-neural-networks.title")} description={t("features.graph-neural-networks.description")}></Feature>
         <Feature icon={dataReliabilityIcon} title={t("features.data-reliability.title")} description={t("features.data-reliability.description")}></Feature>
         <Feature icon={realTimeIcon} title={t("features.real-time-response.title")} description={t("features.real-time-response.description")}></Feature>
-        <Feature icon={contImprovementIcon} title={t("features.cont-improvement.title")} description={t("features.cont-improvement.description")}></Feature>
+        <Feature icon={electronicsIcon} title={t("features.plc-scada-integration.title")} description={t("features.plc-scada-integration.description")}></Feature>
         <Feature icon={riskManagementIcon} title={t("features.risk-management.title")} description={t("features.risk-management.description")}></Feature>
         <Feature icon={environmentImpactIcon} title={t("features.environment-impact.title")} description={t("features.environment-impact.description")}></Feature>
       </div>
