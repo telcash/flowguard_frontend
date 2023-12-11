783c47fc0d736e2acbaa9720ea8f0fedc63520ec
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-11 13:35:46 +0100

    Process responsive

--- src/components/main-slider/MainSlider.jsx
+++ src/components/main-slider/MainSlider.jsx
@@ -8,55 +8,83 @@ const MainSlider = () => {
     const [t] = useTranslation("global");
     return (
         <div className="fg__mainSlider">
-            <Carousel slide={false}>
-                <Carousel.Item className="fg__mainSlider-item">
-                    <img src={dataPreprocessingImg} alt="dummy" />
-                    <Carousel.Caption>
-                        <h3>{t("project-plan.step1.title")}</h3>
-                        <p>{t("project-plan.step1.description")}</p>
-                    </Carousel.Caption>
+            <Carousel slide={false} indicators={false}>
+                <Carousel.Item>
+                    <div className="fg__mainSlider-item">
+                        <div className="fg__mainSlider-item_img">
+                            <img src={dataPreprocessingImg} alt="dummy" />
+                        </div>
+                        <div className="fg__mainSlider-item_info">
+                            <h3>{t("project-plan.step1.title")}</h3>
+                            <p>{t("project-plan.step1.description")}</p>
+                        </div>
+                    </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={dataTopologyImg} alt="dummy" />
-                    <Carousel.Caption>
-                        <h3>{t("project-plan.step2.title")}</h3>
-                        <p>{t("project-plan.step2.description")}</p>
-                    </Carousel.Caption>
+                    <div className="fg__mainSlider-item">
+                        <div className="fg__mainSlider-item_img">
+                            <img src={dataTopologyImg} alt="dummy" />
+                        </div>
+                        <div className="fg__mainSlider-item_info">
+                            <h3>{t("project-plan.step2.title")}</h3>
+                            <p>{t("project-plan.step2.description")}</p>
+                        </div>
+                    </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={normalBehaviourLearnImg} alt="dummy" />
-                    <Carousel.Caption>
-                        <h3>{t("project-plan.step3.title")}</h3>
-                        <p>{t("project-plan.step3.description")}</p>
-                    </Carousel.Caption>
+                    <div className="fg__mainSlider-item">
+                        <div className="fg__mainSlider-item_img">
+                            <img src={normalBehaviourLearnImg} alt="dummy" />  
+                        </div>
+                        <div className="fg__mainSlider-item_info">
+                            <h3>{t("project-plan.step3.title")}</h3>
+                            <p>{t("project-plan.step3.description")}</p>
+                        </div>
+                    </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={realTimeDataImg} alt="dummy" />
-                    <Carousel.Caption>
-                        <h3>{t("project-plan.step4.title")}</h3>
-                        <p>{t("project-plan.step4.description")}</p>
-                    </Carousel.Caption>
+                    <div className="fg__mainSlider-item">
+                        <div className="fg__mainSlider-item_img">
+                            <img src={realTimeDataImg} alt="dummy" />
+                        </div>
+                        <div className="fg__mainSlider-item_info">
+                            <h3>{t("project-plan.step4.title")}</h3>
+                            <p>{t("project-plan.step4.description")}</p>
+                        </div>
+                    </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={sensorsOptimizationImg} alt="dummy" />
-                    <Carousel.Caption>
-                        <h3>{t("project-plan.step5.title")}</h3>
-                        <p>{t("project-plan.step5.description")}</p>
-                    </Carousel.Caption>
+                    <div className="fg__mainSlider-item">
+                        <div className="fg__mainSlider-item_img">
+                            <img src={sensorsOptimizationImg} alt="dummy" />
+                        </div>
+                        <div className="fg__mainSlider-item_info">
+                            <h3>{t("project-plan.step5.title")}</h3>
+                            <p>{t("project-plan.step5.description")}</p>
+                        </div>
+                    </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={integrationTestImg} alt="dummy" />
-                    <Carousel.Caption>
-                        <h3>{t("project-plan.step6.title")}</h3>
-                        <p>{t("project-plan.step6.description")}</p>
-                    </Carousel.Caption>
+                    <div className="fg__mainSlider-item">
+                        <div className="fg__mainSlider-item_img">
+                            <img src={integrationTestImg} alt="dummy" />
+                        </div>
+                        <div className="fg__mainSlider-item_info">
+                            <h3>{t("project-plan.step6.title")}</h3>
+                            <p>{t("project-plan.step6.description")}</p>
+                        </div>
+                    </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={implementationMainteinance} alt="dummy" />
-                    <Carousel.Caption>
-                        <h3>{t("project-plan.step7.title")}</h3>
-                        <p>{t("project-plan.step7.description")}</p>
-                    </Carousel.Caption>
+                    <div className="fg__mainSlider-item">
+                        <div className="fg__mainSlider-item_img">
+                            <img src={implementationMainteinance} alt="dummy" />
+                        </div>
+                        <div className="fg__mainSlider-item_info">
+                            <h3>{t("project-plan.step7.title")}</h3>
+                            <p>{t("project-plan.step7.description")}</p>
+                        </div>
+                    </div>
                 </Carousel.Item>
             </Carousel>
         </div>
