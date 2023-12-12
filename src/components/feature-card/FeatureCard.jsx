f8c8781b87a3939d6c68961ef0f1e573a125fa8a
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-12 20:34:06 +0100

    Agregada animación de flip card en Feature y ajustados los estilos

--- src/components/feature-card/FeatureCard.jsx
+++ src/components/feature-card/FeatureCard.jsx
@@ -11,11 +11,11 @@ const FeatureCard = ({icon, title, description}) => {
                 `fg__featureCard ${hover ? 'rotate-in-ver' : ''}`
             }>
                 <figure className="fg__featureCard-icon">
-                    <img src={icon} alt={title} />
+                    <img className={hover ? 'flipped' : ''} src={icon} alt={title} />
                 </figure>
                 <div className="fg__featureCard-body">
-                    <h3>{title}</h3>
-                    <p>{description}</p>
+                    <h3 className={hover ? 'flipped' : ''}>{title}</h3>
+                    <p className={hover ? 'flipped' : ''}>{description}</p>
                 </div>
             </div>
         </article>
