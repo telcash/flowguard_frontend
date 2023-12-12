b62dead7196af04d15d6d032563b575a414b4ecd
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-12 13:11:03 +0100

    Implementacion inicial de FeatureCard

---
+++ src/components/feature-card/FeatureCard.jsx
@@ -0,0 +1,25 @@
+import React, {useState } from "react";
+import './feature-card.css';
+
+const FeatureCard = ({icon, title, description}) => {
+    const [hover, setHover] = useState(false);
+    return(
+        <article
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}>
+            <div className={
+                `fg__featureCard ${hover ? 'scale-in-center' : ''}`
+            }>
+                <figure className="fg__featureCard-icon">
+                    <img src={icon} alt={title} />
+                </figure>
+                <div className="fg__featureCard-body">
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+        </article>
+    );
+};
+
+export default FeatureCard;
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,11 +4,11 @@ import Feature from '../../components/feature/Feature';
 import neuralNetworkIcon from '../../assets/neural-network.png';
 import dataReliabilityIcon from '../../assets/data-quality.png';
 import realTimeIcon from '../../assets/in-time.png';
-//import contImprovementIcon from '../../assets/improvement.png';
 import riskManagementIcon from '../../assets/risk-management.png';
 import environmentImpactIcon from '../../assets/environment.png';
 import electronicsIcon from '../../assets/electronics.png';
 import { useTranslation } from 'react-i18next';
+import FeatureCard from '../../components/feature-card/FeatureCard';
 
 const Features = () => {
   const [t] = useTranslation("global");
@@ -16,7 +16,8 @@ const Features = () => {
     <div className='fg__features'>
       <h1>{t("features.title")}</h1>
       <div className='fg__features-container'>
-        <Feature icon={neuralNetworkIcon} title={t("features.graph-neural-networks.title")} description={t("features.graph-neural-networks.description")}></Feature>
+        <FeatureCard icon={dataReliabilityIcon} title={t("features.data-reliability.title")} description={t("features.data-reliability.description")}></FeatureCard>
+        {/* <Feature icon={neuralNetworkIcon} title={t("features.graph-neural-networks.title")} description={t("features.graph-neural-networks.description")}></Feature> */}
         <Feature icon={dataReliabilityIcon} title={t("features.data-reliability.title")} description={t("features.data-reliability.description")}></Feature>
         <Feature icon={realTimeIcon} title={t("features.real-time-response.title")} description={t("features.real-time-response.description")}></Feature>
         <Feature icon={electronicsIcon} title={t("features.plc-scada-integration.title")} description={t("features.plc-scada-integration.description")}></Feature>
