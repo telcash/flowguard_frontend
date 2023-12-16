04f4a94b70d1a6e3f992c109a04be01bdb35ec21
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-16 12:09:33 +0100

    Solucionado parcialmente problema de animacion de fondo

--- src/components/feature-card/FeatureCard.jsx
+++ src/components/feature-card/FeatureCard.jsx
@@ -5,14 +5,34 @@ const FeatureCard = ({icon, title, description}) => {
     const [hover, setHover] = useState(false);
     const article = useRef(null);
 
+    const flipcard = () => {
+        setHover(true);
+        article.current.style.zIndex = 0;
+        console.log('se dispara onleave')
+    }
     return(
         <article
             ref={article}
-            onMouseEnter={(e) => {
-                setHover(true);
+            onMouseEnter={() => {
+                flipcard();
             }}
-            onMouseLeave={() => {
-                setHover(false);
+            onMouseLeave={(e) => {
+                console.log(`x: ${e.clientX}`);
+                console.log(`left: ${article.current.getBoundingClientRect().left}`);
+                console.log(`right: ${article.current.getBoundingClientRect().right}`);
+                console.log(`y: ${e.clientY}`);
+                console.log(`top: ${article.current.getBoundingClientRect().top}`);
+                console.log(`bottom: ${article.current.getBoundingClientRect().bottom}`);
+                if(
+                    !(e.clientX > article.current.getBoundingClientRect().left &&
+                    e.clientX < article.current.getBoundingClientRect().right &&
+                    e.clientY > article.current.getBoundingClientRect().top &&
+                    e.clientY < article.current.getBoundingClientRect().bottom)
+                ) {
+                    setHover(false);
+                }
+                article.current.style.zIndex = 1;
+                console.log('se dispara onenter');
             }}
         >
             <div className={
