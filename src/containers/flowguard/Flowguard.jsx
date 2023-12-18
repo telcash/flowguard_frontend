610ea66ab0eb41f451f30e340ebc0c5ba636f4d5
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-18 13:54:15 +0100

    Modificada informacion en Flowguard

--- src/containers/flowguard/Flowguard.jsx
+++ src/containers/flowguard/Flowguard.jsx
@@ -8,15 +8,16 @@ const Flowguard = () => {
             <div className="fg__flowguard-header">
                 <h2>FLOWGUARD</h2>
                 <p>
-                    Flowguard es una potente herramienta informática SaaS formada por las tecnologías de Ciberseguridad NIS2, Inteligencia Artificial y Blockchain  
+                Es una herramienta informática formada por las tecnologías de Ciberseguridad NIS2, Inteligencia Artificial y Blockchain,
+                destinada a proteger de Ciberataques a las infraestructuras críticas en los sectores del Agua, Energía y Espacios públicos
                 </p> 
             </div>
             <div className="fg__flowguard-info">
                 <div className="fg__flowguard-info-text-left">
                     <ul>
-                        <li>Flowguard está destinada a ser la tecnología que protege de Ciberataques a las infraestructuras críticas en los sectores del Agua, Energía y Espacios públicos</li>
-                        <li>Integrable con las tecnologías de gestión de procesos: PLCs y SCADAS: escalabilidad total</li>
-                        <li>Predicción de anomalías funcionales de los procesos que perjudiquen a la producción y a la seguridad</li>
+                        <li>Provee detección temprana y respuesta rápida a incidentes.</li>
+                        <li>Blockchain ofrece un registro transparente e inmutable, reduce el riesgo de manipulación de datos en infraestructuras críticas y aumenta la resiliencia frente a ataques centralizados</li>
+                        <li>Ofrece documentación y reportes conforme a NIS2  (Network and Information Security Directive) y análisis en tiempo real para auditoría y evaluación de seguridad</li>
                     </ul>
                 </div>
                 <div className="fg__flowguard-info-logo">
@@ -24,9 +25,9 @@ const Flowguard = () => {
                 </div>
                 <div className="fg__flowguard-info-text-right">
                     <ul>
-                        <li>Flowguard está destinada a ser la tecnología que protege de Ciberataques a las infraestructuras críticas en los sectores del Agua, Energía y Espacios públicos</li>
-                        <li>Integrable con las tecnologías de gestión de procesos: PLCs y SCADAS: escalabilidad total</li>
-                        <li>Predicción de anomalías funcionales de los procesos que perjudiquen a la producción y a la seguridad</li>
+                        <li>Predice anomalías funcionales en los procesos que perjudiquen a la producción y a la seguridad</li>
+                        <li>Se integra con las tecnologías de gestión de procesos: PLCs y SCADAS, permitiendo un rápido despliegue de la herramienta</li>
+                        <li>Analiza en tiempo real los patrones óptimos de funcionamiento, detectando alteraciones de estos en caso de Ciberataque</li>
                     </ul>
                 </div>
             </div>
