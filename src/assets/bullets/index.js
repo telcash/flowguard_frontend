781894ee2722b9b9b6b1d1d6bf21e1c8993f18fe
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-21 21:08:34 +0100

    Ajustados estilos en Flwguard

---
+++ src/assets/bullets/index.js
@@ -0,0 +1,2 @@
+export { default as checkIcon } from './check.png';
+export { default as dangerIcon } from './danger.png';
--- src/containers/flowguard/Flowguard.jsx
+++ src/containers/flowguard/Flowguard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './flowguard.css';
 import logo from "../../assets/logo.png";
+import { checkIcon, dangerIcon } from '../../assets/bullets';
 
 const Flowguard = () => {
     return (
@@ -8,9 +9,18 @@ const Flowguard = () => {
             <div className="fg__flowguard-info">
                 <div className="fg__flowguard-info-text-left">
                     <ul>
-                        <li>Provee detección temprana y respuesta rápida a incidentes.</li>
-                        <li>Blockchain ofrece un registro transparente e inmutable, reduce el riesgo de manipulación de datos en infraestructuras críticas y aumenta la resiliencia frente a ataques centralizados</li>
-                        <li>Ofrece documentación y reportes conforme a NIS2  (Network and Information Security Directive) y análisis en tiempo real para auditoría y evaluación de seguridad</li>
+                        <li>
+                            <img src={dangerIcon} alt="" />
+                            <h6>Provee detección temprana y respuesta rápida a incidentes</h6>
+                        </li>
+                        <li>
+                            <img src={dangerIcon} alt="" />
+                            <h6>Blockchain ofrece un registro transparente e inmutable, reduce el riesgo de manipulación de datos en infraestructuras críticas y aumenta la resiliencia frente a ataques centralizados</h6>
+                        </li>
+                        <li>
+                            <img src={dangerIcon} alt="" />
+                            <h6>Ofrece documentación y reportes conforme a NIS2  (Network and Information Security Directive) y análisis en tiempo real para auditoría y evaluación de seguridad</h6>
+                        </li>
                     </ul>
                 </div>
                 <div className="fg__flowguard-info-logo">
@@ -19,9 +29,18 @@ const Flowguard = () => {
                 </div>
                 <div className="fg__flowguard-info-text-right">
                     <ul>
-                        <li>Predice anomalías funcionales en los procesos que perjudiquen a la producción y a la seguridad</li>
-                        <li>Se integra con las tecnologías de gestión de procesos: PLCs y SCADAS, permitiendo un rápido despliegue de la herramienta</li>
-                        <li>Analiza en tiempo real los patrones óptimos de funcionamiento, detectando alteraciones de estos en caso de Ciberataque</li>
+                        <li>
+                            <img src={checkIcon} alt="" />
+                            <h6>Predice anomalías funcionales en los procesos que perjudiquen a la producción y a la seguridad</h6>
+                        </li>
+                        <li>
+                            <img src={checkIcon} alt="" />
+                            <h6>Se integra con las tecnologías de gestión de procesos: PLCs y SCADAS, permitiendo un rápido despliegue de la herramienta</h6>
+                        </li>
+                        <li>
+                            <img src={checkIcon} alt="" />
+                            <h6>Analiza en tiempo real los patrones óptimos de funcionamiento, detectando alteraciones de estos en caso de Ciberataque</h6>
+                        </li>
                     </ul>
                 </div>
             </div>
