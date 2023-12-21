9b2b91cb68284e7e25e791459c2c6104e2d41db4
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-21 09:32:37 +0100

    Agregados estilos a Footer, parte de arriba

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,15 +16,26 @@ const Footer = () => {
 
   return (
     <div className='fg__footer'>
-        <div className="fg__footer-cta">
-            <Cta data={showForm}></Cta>
+        <div className="fg__footer-top">
+          <div className='fg__footer-top-quote'>
+            <div className='fg__footer-top-quote-info'>
+              <p>“Creo que el próximo Pearl Harbor o el próximo 11 de septiembre será cibernético, y nos enfrentamos a una vulnerabilidad en todos sistemas, pero el agua es uno de los más críticos y creo que uno de los más vulnerables”</p>
+              <h3>- Angus King. Senador de EEUU</h3>
+            </div>
+          </div>
+          <div className='fg__footer-top-cta'>
+            <h2>Comienza ahora</h2>
+            <div className='fg__footer-top-cta-form'>
+                <Cta data={showForm}></Cta>
+            </div>
+          </div>
         </div>
         {isFormVisible && (
             <div className="fg__hero-cta-form">
               <CtaForm onClose={hideForm} email={email}/>
             </div>
           )}
-        <div className='fg__footer-info'>
+        <div className='fg__footer-bottom'>
             Info
         </div>
     </div>
