788f4e0c245333635980d20155e5a14becf75f36
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-12-28 08:06:55 +0100

    Ajustado Flex en Partners

--- src/containers/partners/Partners.jsx
+++ src/containers/partners/Partners.jsx
@@ -10,24 +10,30 @@ const Partners = () => {
         <div className="fg__partners">
             <h1>{t("partners.title")}</h1>
             <div className="fg__partners-logos">
-                <PartnerLogo
-                    name={'Airtrace'}
-                    logoGray={airtraceLogoGray}
-                    logoColor={airtraceLogoColor}
-                    url={"https://airtrace.io"}
-                />
-                <PartnerLogo
-                    name={'Neoradix'}
-                    logoGray={neoradixLogoGray}
-                    logoColor={neoradixLogoColor}
-                    url={"https://neoradixsolutions.com"}
-                />
-                <PartnerLogo
-                    name={'Securit'}
-                    logoGray={securitLogoGray}
-                    logoColor={securitLogoColor}
-                    url={"https://securit-project.eu"}
-                />
+                <div className="fg__partners-logos-logo">
+                    <PartnerLogo
+                        name={'Airtrace'}
+                        logoGray={airtraceLogoGray}
+                        logoColor={airtraceLogoColor}
+                        url={"https://airtrace.io"}
+                    />
+                </div>
+                <div className="fg__partners-logos-logo">
+                    <PartnerLogo
+                        name={'Neoradix'}
+                        logoGray={neoradixLogoGray}
+                        logoColor={neoradixLogoColor}
+                        url={"https://neoradixsolutions.com"}
+                    />
+                </div>
+                <div className="fg__partners-logos-logo">
+                    <PartnerLogo
+                        name={'Securit'}
+                        logoGray={securitLogoGray}
+                        logoColor={securitLogoColor}
+                        url={"https://securit-project.eu"}
+                    />
+                </div>
             </div>
         </div>
     );
